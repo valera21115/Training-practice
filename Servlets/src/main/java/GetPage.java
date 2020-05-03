8d2815a740cb09e115a370403b6ea408d06d0979
valera21115 <valera.potapovich@gmail.com>
2020-05-04 02:59:59 +0300

    add task5

---
+++ Servlets/src/main/java/GetPage.java
@@ -0,0 +1,12 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class GetPage extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       request.getRequestDispatcher("page.html").forward(request,response);
+}
+}
